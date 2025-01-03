b250efd584f7c57138a3ca1d4eb13e81aacaf5d1
Brian Kemper <bkemper@gmail.com>
2025-01-03 12:50:38 -0700

    .

--- tests/index_test.ts
+++ tests/index_test.ts
@@ -14,21 +14,88 @@ function generateSnapshotPath(testFilePath: string | undefined): string {
   return join(base, 'snaps', `${filename}.snap.cjs`);
 }
 
-suite('s3-simple', () => {
-  test('should sign a simple GET request', async (t) => {
-    const s3 = new S3Client({
-      accessKeyId: 'asdID',
-      secretAccessKey: 'asdSecret',
-    });
+const s3 = new S3Client({
+  accessKeyId: 'asdID',
+  secretAccessKey: 'asdSecret',
+});
 
-    s3.date = () => new Date('2021-01-01T00:00:00Z');
+s3.date = () => new Date('2021-01-01T00:00:00Z');
 
+suite('s3-simple GET', () => {
+  test('should sign a request', async (t) => {
     const r = s3.sign({
       method: 'GET',
       bucket: 'asdBucket',
       key: 'asdKey',
       extraHeaders: {},
       file: null,
+    });
+    t.assert.snapshot(r);
+  });
+  test('should sign a request with extra headers', async (t) => {
+    const r = s3.sign({
+      method: 'GET',
+      bucket: 'asdBucket',
+      key: 'asdKey',
+      extraHeaders: {
+        'x-amz-acl': 'public-read',
+      },
+      file: null,
+    });
+    t.assert.snapshot(r);
+  });
+});
+
+suite('s3-simple DELETE', () => {
+  test('should sign a request', async (t) => {
+    const r = s3.sign({
+      method: 'DELETE',
+      bucket: 'asdBucket',
+      key: 'asdKey',
+      extraHeaders: {},
+      file: null,
+    });
+    t.assert.snapshot(r);
+  });
+  test('should sign a request with extra headers', async (t) => {
+    const r = s3.sign({
+      method: 'DELETE',
+      bucket: 'asdBucket',
+      key: 'asdKey',
+      extraHeaders: {
+        'x-amz-acl': 'public-read',
+      },
+      file: null,
+    });
+    t.assert.snapshot(r);
+  });
+});
+
+suite('s3-simple PUT', () => {
+  test('should sign a request', async (t) => {
+    const r = s3.sign({
+      method: 'PUT',
+      bucket: 'asdBucket',
+      key: 'asdKey',
+      extraHeaders: {},
+      file: {
+        content: 'asdzasdi',
+      },
+    });
+    t.assert.snapshot(r);
+  });
+  test('should sign a request with extra headers', async (t) => {
+    const r = s3.sign({
+      method: 'PUT',
+      bucket: 'asdBucket',
+      key: 'asdKey',
+      extraHeaders: {
+        'x-amz-acl': 'public-read',
+      },
+      file: {
+        content: 'asdzasdi',
+        contentType: 'text/plain',
+      },
     });
     t.assert.snapshot(r);
   });
--- tests/snaps/index_test.snap.cjs
+++ tests/snaps/index_test.snap.cjs
@@ -1,8 +1,71 @@
-exports[`s3-simple > should sign a simple GET request 1`] = `
+exports[`s3-simple DELETE > should sign a request 1`] = `
+{
+  "signedHeaders": {
+    "Authorization": "AWS asdID:oBTx/gWFghH4Y/Wq4+rxc58tTbk=",
+    "date": "20210101T000000Z"
+  },
+  "reqURL": "https://asdBucket.s3.amazonaws.com/asdKey",
+  "cleanedKey": "asdKey",
+  "cleanedBucket": "asdBucket"
+}
+`;
+
+exports[`s3-simple DELETE > should sign a request with extra headers 1`] = `
+{
+  "signedHeaders": {
+    "Authorization": "AWS asdID:hCITid406IipyThf1RtUGzi7Zjc=",
+    "date": "20210101T000000Z"
+  },
+  "reqURL": "https://asdBucket.s3.amazonaws.com/asdKey",
+  "cleanedKey": "asdKey",
+  "cleanedBucket": "asdBucket"
+}
+`;
+
+exports[`s3-simple GET > should sign a request 1`] = `
 {
   "signedHeaders": {
     "Authorization": "AWS asdID:gyyV7uuNc0QwltEgiLriKRlR2+k=",
     "date": "20210101T000000Z"
+  },
+  "reqURL": "https://asdBucket.s3.amazonaws.com/asdKey",
+  "cleanedKey": "asdKey",
+  "cleanedBucket": "asdBucket"
+}
+`;
+
+exports[`s3-simple GET > should sign a request with extra headers 1`] = `
+{
+  "signedHeaders": {
+    "Authorization": "AWS asdID:Z/1jA8NqmXG7R99fuguhS76rhNA=",
+    "date": "20210101T000000Z"
+  },
+  "reqURL": "https://asdBucket.s3.amazonaws.com/asdKey",
+  "cleanedKey": "asdKey",
+  "cleanedBucket": "asdBucket"
+}
+`;
+
+exports[`s3-simple PUT > should sign a request 1`] = `
+{
+  "signedHeaders": {
+    "Authorization": "AWS asdID:0vyT1wKpEwV2f8hXiaFMnDC4aSg=",
+    "date": "20210101T000000Z",
+    "Content-MD5": "lqnTkjmDigTWGjuDuV3Kng=="
+  },
+  "reqURL": "https://asdBucket.s3.amazonaws.com/asdKey",
+  "cleanedKey": "asdKey",
+  "cleanedBucket": "asdBucket"
+}
+`;
+
+exports[`s3-simple PUT > should sign a request with extra headers 1`] = `
+{
+  "signedHeaders": {
+    "Authorization": "AWS asdID:xPXxFguantuHbYs/RIZaoEzbBuc=",
+    "date": "20210101T000000Z",
+    "Content-MD5": "lqnTkjmDigTWGjuDuV3Kng==",
+    "Content-Type": "text/plain"
   },
   "reqURL": "https://asdBucket.s3.amazonaws.com/asdKey",
   "cleanedKey": "asdKey",
