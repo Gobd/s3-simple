1e28424484a34655be5d7204bdadac40d8106e65
Brian Kemper <bkemper@gmail.com>
2025-01-03 11:21:38 -0700

    .

--- build.sh
+++ build.sh
@@ -4,10 +4,17 @@ set -ex
 
 rm -rf ./dist
 
-sed -i.bak 's/public cleanPath(/private cleanPath(/' filename
-sed -i.bak 's/public date(/private date(/' filename
-sed -i.bak 's/public sign(/private sign(/' filename
-
 npm run lint
 npm run ts-check
+
+sed -i.bak 's/public cleanPath(/private cleanPath(/' index.ts
+sed -i.bak 's/public date(/private date(/' index.ts
+sed -i.bak 's/public sign(/private sign(/' index.ts
+
 npm run build
+
+sed -i.bak 's/private cleanPath(/public cleanPath(/' index.ts
+sed -i.bak 's/private date(/public date(/' index.ts
+sed -i.bak 's/private sign(/public sign(/' index.ts
+
+rm -rf index.ts.bak
